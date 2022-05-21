29d99a647506adae730e365e89d1ce2bead860a6
Bo Chen <bochen1993cs@gmail.com>
2022-05-21 14:36:25 -0500

    Update prune.py

--- prune.py
+++ prune.py
@@ -790,7 +790,7 @@ class FisherPruningHook():
         """
         # same group same softmask
         module.trained_mask = self.trained_mask
-        limit = float(1e-4)
+        limit = float(1)
         module.noise_mask = self.noise_mask
         module.finetune = not pruning
         if type(module).__name__ == 'Conv2d':
