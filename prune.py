9f7ab7afbec721df4491bb8126806e7233a5e94f
Bo Chen <bochen1993cs@gmail.com>
2022-06-06 14:39:42 -0700

    Update prune.py

--- prune.py
+++ prune.py
@@ -514,14 +514,13 @@ class FisherPruningHook():
             for module in self.groups[group]:
                 weight_list = torch.cat((weight_list,module.weight.view(-1)))
                 
-        sorted, indices = weight_list.sort(dim=0)
-        num_groups,mult,noise_decay = 2,1,1e-1
-        split_size = len(weight_list)//num_groups + 1
-        ind_groups = torch.split(indices, split_size)
-        # weight should be adjusted/scaled
-        for ind_group in ind_groups:
-            weight_list[ind_group] *= mult
-            mult *= noise_decay
+        #sorted, indices = weight_list.sort(dim=0)
+        #num_groups,mult,noise_decay = 2,1,1e-1
+        #split_size = len(weight_list)//num_groups + 1
+        #ind_groups = torch.split(indices, split_size)
+        #for ind_group in ind_groups:
+        #    weight_list[ind_group] *= mult
+        #    mult *= noise_decay
         
         total_penalty = self.penalty[0]/abs(self.penalty[0]) * self.penalty[1] * torch.norm(weight_list,p=abs(self.penalty[0]))
         return total_penalty
