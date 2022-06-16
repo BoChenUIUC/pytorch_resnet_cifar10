b99971f8473433f2fb4a99ac14f44f1b701d3977
Bo Chen <bochen1993cs@gmail.com>
2022-06-16 02:52:20 -0700

    Update prune.py

--- prune.py
+++ prune.py
@@ -214,7 +214,7 @@ class FisherPruningHook():
                 if self.penalty is not None:
                     save_dir = f'metrics/L{int(-math.log10(max(1e-8,abs(self.penalty[0]))))}_{int(-math.log10(max(1e-8,abs(self.penalty[1]))))}_{int(-math.log10(max(1e-8,abs(self.penalty[2]))))}_{int(-math.log10(max(1e-8,abs(self.penalty[3]))))}/'
                 else:
-                    save_dir = f'metrics/expqnt2/'
+                    save_dir = f'metrics/expqnt4/'
                 if not os.path.exists(save_dir):
                     os.makedirs(save_dir)
                 # fisher
