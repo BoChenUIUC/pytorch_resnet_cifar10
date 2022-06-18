ac4fbfa109b99a5bb58ca461224c1be63c7bee6a
Bo Chen <bochen1993cs@gmail.com>
2022-06-17 21:41:54 -0700

    Update prune.py

--- prune.py
+++ prune.py
@@ -525,7 +525,7 @@ class FisherPruningHook():
             dist = torch.abs(torch.log(torch.abs(x).unsqueeze(-1)/bins))
             _,min_idx = dist.min(dim=-1)
             offset = bins[min_idx] - torch.abs(x)
-            x = torch.sign(x) * (torch.abs(x) + decay_factor * bins[min_idx])
+            x = torch.sign(x) * (torch.abs(x) + decay_factor * offset)
             all_err = torch.abs(torch.log(bins[min_idx]/torch.abs(x)))
             self.ista_err += all_err.sum()
             # calculating err for each bin
