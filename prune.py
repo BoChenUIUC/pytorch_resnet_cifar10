96c9312a4eaf524edea221c9a934c0f1507574d0
Bo Chen <bochen1993cs@gmail.com>
2022-06-17 22:37:56 -0700

    Update prune.py

--- prune.py
+++ prune.py
@@ -521,6 +521,7 @@ class FisherPruningHook():
             x = torch.clamp(torch.abs(x), min=1e-8) * torch.sign(x)
             #bins = torch.FloatTensor([1e-8,1e-6,1e-4,1e-2,1,1e2,1e4,1e6]).to(x.device)
             bins = torch.pow(10,torch.tensor([-2,-1,0,1,2])).to(x.device)
+            print(bins)
             decay_factor = 1e-3
             dist = torch.abs(torch.log10(torch.abs(x).unsqueeze(-1)/bins))
             _,min_idx = dist.min(dim=-1)
