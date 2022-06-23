f7033eab4a9d72b34c84f8c34e25da3cf8aea03f
Bo Chen <bochen1993cs@gmail.com>
2022-06-23 15:30:29 -0700

    Update prune.py

--- prune.py
+++ prune.py
@@ -368,10 +368,10 @@ class FisherPruningHook():
         ch_start = 0
         for module, name in self.conv_names.items():
             # find child of this module
-            if not hasattr(module, 'child'):
-                continue
             ch_len = len(module.in_mask)
-            module.child.in_mask[:] = all_masks[ch_start:ch_start+ch_len]
+            if hasattr(module, 'child'):
+                child = self.name2module[module.child]
+                child.in_mask[:] = all_masks[ch_start:ch_start+ch_len]
             ch_start += ch_len
 
     def init_flops_acts(self):
