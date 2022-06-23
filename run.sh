18bd1dfcff0bb2bdf278f4e81a1b0af478b4b783
Bo Chen <bochen1993cs@gmail.com>
2022-06-23 16:15:26 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ for model in resnet56 # resnet20 resnet32 resnet44 resnet110 resnet1202
 do
     echo "python -u trainer.py  --arch=$model  --save-dir=save_$model |& tee -a log_$model"
 	#python -u trainer.py --prune=save_resnet56/best.th  --arch=$model  --save-dir=save_$model |& tee -a log_$model
-	python -u trainer.py --prune=save_resnet56/best_logq4.th  --arch=$model --evaluate
+	python -u trainer.py --prune=save_resnet56/best_loqg4.th  --arch=$model --evaluate
 done
